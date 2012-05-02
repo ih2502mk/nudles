507e70fd27c6cf20b3911189ef201fa8590a736b
Ihor Marusyk <ih2502mk@gmail.com>
2012-05-02 22:12:11 +0300

    Tests for containers

--- iter3/test.js
+++ iter3/test.js
@@ -1,15 +1,23 @@
+var assert = require('assert');
 var Containers = require('./containers.js').Containers;
-
-Containers.push({
-  name: 'hello',
-  type: 'NestContainer',
-  nested: ['goodbye', 'sionara']
-});
 
 Containers.push({
   name: 'goodbye',
   type: 'MarkupContainer',
   filler: "Good Bye Filler string"
+});
+
+assert(Containers.containers['goodbye'], 'Push a new container to a collection');
+
+Containers.render('goodbye', function (err, result) {
+  assert.equal(result, "<span>Good Bye Filler string</span>");
+  console.log("MarkupContainer with default options - passed.")
+});
+
+Containers.push({
+  name: 'hello',
+  type: 'NestContainer',
+  nested: ['goodbye', 'sionara']
 });
 
 Containers.push({
